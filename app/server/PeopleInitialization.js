3b2fc0c85a3e594f6f89738f0a1bc8016ca1620e
Kyle <kleve84@hawaii.edu>
2017-10-09 18:20:10 -1000

    Added Simple Schema

--- app/server/PeopleInitialization.js
+++ app/server/PeopleInitialization.js
@@ -5,5 +5,6 @@ if (people.find().count() === 0) {
   people.insert({ first: "Joanne", last: "Amberg", age: 58, city: "Kailua" });
   people.insert({ first: "Jenna", last: "Corin", age: 31, city: "Boulder Creek" });
   people.insert({ first: "Katie", last: "Kai", age: 25, city: "Palo Alto" });
-  people.insert({ first: "Doris", last: "Johnson", age: 93, city: "Naples" }); 
+  people.insert({ first: "Doris", last: "Johnson", age: 93, city: "Naples" });
 }
+// people.insert({ animal: "Dog", name: "Koa", city: "Pearl City" });
